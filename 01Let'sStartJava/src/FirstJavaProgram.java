982107dbc74cfc8b848c736a5ec67f23fa1bf82e
seo youngho <youngho0983@naver.com>
2021-02-21 15:53:48 +0900

    first

---
+++ 01Let'sStartJava/src/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+
+public class FirstJavaProgram {
+	public static void main(String[] args) {
+		// 반가운 자바의 시작 다시 시작 해보다 새 마음 새뜻으로 완벽하게 정리하자
+		
+		System.out.println("welcome to Java");
+		System.out.println("First? Java Program");
+	}
+}
